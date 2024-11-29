a76c70874e051df0fc36b818591d0ebd6cf3dc5f
juanda64 <56767238+juanda64@users.noreply.github.com>
2024-11-28 23:11:19 -0500

    creacion de jwt_secret

--- BackTuVideo/middleware/auth.js
+++ BackTuVideo/middleware/auth.js
@@ -1,23 +1,17 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
 
-const protect = async (req, res, next) => {
-    let token;
-
-    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-        try {
-            token = req.headers.authorization.split(' ')[1];
-            const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-            req.user = await User.findById(decoded.id).select('-password');
-            next();
-        } catch (err) {
-            return res.status(401).json({ message: 'Token no válido' });
-        }
+const protect = (req, res, next) => {
+    const token = req.headers.authorization?.split(' ')[1]; // "Bearer TOKEN"
+    if (!token) {
+        return res.status(401).json({ message: 'No autorizado, falta el token' });
     }
 
-    if (!token) {
-        return res.status(401).json({ message: 'No autorizado, falta token' });
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded; // Agregar la información del usuario al request
+        next();
+    } catch (error) {
+        return res.status(401).json({ message: 'Token inválido o expirado' });
     }
 };
 
